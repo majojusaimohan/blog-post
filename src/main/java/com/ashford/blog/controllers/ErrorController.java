423c830f5aa75193315dc16883dac3bc6600160d
LAPTOP-QA62HH1U\saimo <saimohanmajoju@gmail.com>
2026-01-17 16:10:13 -0500

    Security implemented

--- src/main/java/com/ashford/blog/controllers/ErrorController.java
+++ src/main/java/com/ashford/blog/controllers/ErrorController.java
@@ -4,6 +4,7 @@ import com.ashford.blog.domain.dtos.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +42,15 @@ public class ErrorController {
                 .message(ex.getMessage())
                 .build();
         return new ResponseEntity<>(error,HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiErrorResponse> handleBadCredentialsException(BadCredentialsException ex) {
+        log.error("Caught BadCredentialsException",ex);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message("Incorrect username or password")
+                .build();
+        return new ResponseEntity<>(error,HttpStatus.UNAUTHORIZED);
     }
 }
